3385479714aad7e3b95dc75f666d7af7f0516b38
AQSA-ABDULLAH <aqsaabdullah38403@gmail.com>
2023-07-11 05:48:57 -0700

    Add Styling

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import '../node_modules/bootstrap/dist/js/bootstrap.bundle';
 import '../node_modules/bootstrap/dist/js/bootstrap.bundle.min.js';
 const App = () => {
   return (
-    <div style={{ background: 'linear-gradient( #f1f1f1, #dddddd)', height: '1000px'}} >
+    <div style={{ background: 'linear-gradient( #f1f1f1, #dddddd)', height: '2000px'}} >
       <h1 className='text-center p-5 mb-2'>TODO LIST IN REACT JS</h1>
       <AddInList />
     </div>
